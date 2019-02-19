2bb0d4ca9f0e009f5a62264f66c1febbd73f94c2
Daniel Cote <dccote@novajo.ca>
2019-02-19 10:19:58 -0500

    Donnees et images

--- ATELIER/FFT/generateData.py
+++ ATELIER/FFT/generateData.py
@@ -3,17 +3,19 @@ from numpy.random import *
 import matplotlib.pyplot as plt
 from numpy.fft import *
 
+filename="hene-short-200.txt"
 N = 200
 xMin = -10
 xMax = 10
 x1 = np.linspace(xMin, xMax, N)
-
 noise = random(N)*0.1
 y1 = np.cos(2 * np.pi / 0.632 * x1)+noise
-np.savetxt("hene-short-200.txt", np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
 
-x1 = np.loadtxt("hene-short-200.txt", usecols=(0))
-y1 = np.loadtxt("hene-short-200.txt", usecols=(1))
+np.savetxt(filename, np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
+
+# Pour etudiants
+x1 = np.loadtxt(filename, usecols=(0))
+y1 = np.loadtxt(filename, usecols=(1))
 spectre = fft(y1)
 fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
 axes.plot(x1, y1, 'o-')
@@ -22,41 +24,46 @@ axesFFT.plot(x1, spectre, 'o-')
 axes.set_title("Spectre")
 plt.show()
 
+x1 = np.loadtxt(filename, usecols=(0))
+y1 = np.loadtxt(filename, usecols=(1))
+spectre = abs(fft(y1))
+fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
+axes.plot(x1, y1, 'o-')
+axes.set_title("Donnees")
+axesFFT.plot(x1, spectre, 'o-')
+axes.set_title("Spectre (valeur absolue)")
+plt.show()
 
 
+filename="hene-short-2000.txt"
 N = 2000
 xMin = -10
 xMax = 10
 x1 = np.linspace(xMin, xMax, N)
 noise = random(N)*0.1
 y1 = np.cos(2 * np.pi / 0.632 * x1)+noise
-np.savetxt("hene-short-2000.txt", np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
-spectre = fft(y1)
+np.savetxt(filename, np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
+
+x1 = np.loadtxt(filename, usecols=(0))
+y1 = np.loadtxt(filename, usecols=(1))
+spectre = abs(fft(y1))
 fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
 axes.plot(x1, y1, 'o-')
+axes.set_title("Donnees")
 axesFFT.plot(x1, spectre, 'o-')
+axes.set_title("Spectre (valeur absolue)")
 plt.show()
 
-N = 2000
+filename="hene-long-2000.txt"
 xMin = -100
 xMax = 100
 x1 = np.linspace(xMin, xMax, N)
 noise = random(N)*0.1
 y1 = np.cos(2 * np.pi / 0.632 * x1)+noise
-np.savetxt("hene-long-2000.txt", np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
-spectre = fft(y1)
-fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
-axes.plot(x1, y1, 'o-')
-axesFFT.plot(x1, spectre, 'o-')
-plt.show()
+np.savetxt(filename, np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
 
-N = 2000
-xMin = -100
-xMax = 100
-x1 = np.linspace(xMin, xMax, N)
-noise = random(N)*0.01
-y1 = np.exp(-x1*x1/4)*np.sin(2 * np.pi / 0.540 * x1)/(2 * np.pi / 0.540 * x1)+noise
-np.savetxt("white-2000.txt", np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
+x1 = np.loadtxt(filename, usecols=(0))
+y1 = np.loadtxt(filename, usecols=(1))
 spectre = abs(fft(y1))
 fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
 axes.plot(x1, y1, 'o-')
@@ -64,13 +71,31 @@ axesFFT.plot(x1, spectre, 'o-')
 plt.show()
 
 
+filename="white-2000.txt"
+N = 2000
+xMin = -100
+xMax = 100
+x1 = np.linspace(xMin, xMax, N)
+noise = random(N)*0.01
+y1 = np.exp(-x1*x1/4)*np.sin(2 * np.pi / 0.540 * x1)/(2 * np.pi / 0.540 * x1)+noise
+np.savetxt(filename, np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
+
+x1 = np.loadtxt(filename, usecols=(0))
+y1 = np.loadtxt(filename, usecols=(1))
+spectre = abs(fft(y1))
+fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
+axes.plot(x1, y1, 'o-')
+axesFFT.plot(x1, spectre, 'o-')
+plt.show()
+
+filename="sodium-20000.txt"
 N = 20000
 xMin = -1500
 xMax = 1500
 x1 = np.linspace(xMin, xMax, N)
 noise = random(N)*0.01
 y1 = (np.sin(2 * np.pi / 0.589 * x1)+np.sin(2 * np.pi / 0.5896 * x1))+noise
-np.savetxt("sodium-20000.txt", np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
+np.savetxt(filename, np.transpose((x1,y1)),header="#Interferogramme He-Ne\n\n# Position\tIntensite\n\n")
 spectre = abs(fft(y1))
 fig, (axes, axesFFT) = plt.subplots(2,1,figsize=(10, 7))
 axes.plot(x1, y1, '-')
