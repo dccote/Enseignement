1014a2edb2a62bcf60287f1ea179cb3a0806c003
Daniel Côté <dccote@novajo.ca>
2020-03-20 22:13:13 -0400

    Create diffraction.py

---
+++ ATELIER/FFT-Diffraction/diffraction.py
@@ -0,0 +1,11 @@
+import numpy as np
+from numpy.random import *
+import matplotlib.pyplot as plt
+from numpy.fft import *
+
+""" Ce script Python propage la lumière incidente sur une fente
+carrée, circulaire et arbitraire en utilisant le formalisme de la 
+diffraction de Fraunhofer.
+
+"""
+
